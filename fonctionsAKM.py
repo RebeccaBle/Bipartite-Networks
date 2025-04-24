208948b06df12428e47da925c0ebbbaca347ef86
gillesf <gilles.fevry@ensae.fr>
2025-04-24 10:33:17 +0000

    mini cgts

--- fonctionsAKM.py
+++ fonctionsAKM.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error
 import statsmodels.api as sm
+from tqdm import tqdm
 
 class graph_AKM:
 
@@ -23,7 +24,7 @@ class graph_AKM:
         self.matrice_distance = None
         self.lien = None
     
-    def create_link(self, effet_pat, effet_doc, beta_lien, show=True):
+    def create_link(self, effet_pat=None, effet_doc=None, beta_lien=None, show=True):
 
         #1 matrice distance
 
@@ -33,10 +34,12 @@ class graph_AKM:
                 self.matrice_distance[i,j] = np.sqrt((self.position_patient[1][i]-self.position_docteur[1][j])**2+(self.position_patient[0][i]-self.position_docteur[0][j])**2)
 
         #2 matrice des liens
-
-        self.beta_lien= beta_lien
-        self.effet_doc = effet_doc
-        self.effet_pat = effet_pat
+        if beta_lien is not None:
+            self.beta_lien= beta_lien
+        if effet_doc is not None:
+            self.effet_doc = effet_doc
+        if effet_pat is not None:
+            self.effet_pat = effet_pat
         self.lien = np.zeros((self.nombre_patient, self.nombre_docteur))
         for j in range(self.nombre_docteur):
             for i in range(self.nombre_patient):
@@ -76,12 +79,16 @@ class graph_AKM:
         plt.legend()
         plt.show()
 
-    def solve_model(self, alpha, psi, constente, beta):
+    def solve_model(self, alpha=None, psi=None, constente=None, beta=None):
 
-        self.alpha = alpha
-        self.psi = psi
-        self.beta = beta
-        self.constente = constente
+        if alpha is not None:
+            self.alpha = alpha
+        if psi is not None:
+            self.psi = psi
+        if beta is not None:
+            self.beta = beta
+        if constente is not None:
+            self.constente = constente
 
         #1 Create prices
 
@@ -161,6 +168,42 @@ class graph_AKM:
             print(f"Le mse_psi vaut: {mse_psi}")
 
         return (mse_prix, mse_alpha, mse_psi)
+
+    def sparcity(self, beta_lien_min, beta_lien_max, nb_points):
+        t=np.linspace(beta_lien_min,beta_lien_max, nb_points)
+        x=[]
+        y=[]
+        z=[]
+        s=[]
+        for i in tqdm(t):
+            self.create_link(self.effet_pat, self.effet_doc, i, show=False)
+            perfs=self.show_perf(self.alpha, self.psi, self.constente, self.beta, show=False)
+            x.append(perfs[0])
+            y.append(perfs[1])
+            z.append(perfs[2])
+            s.append(int(self.lien.sum())/(self.nombre_patient*self.nombre_docteur))
+        
+        data = list(zip(s, x, y, z))
+
+        # Trier par x
+        data_sorted = sorted(data, key=lambda t: t[0])
+
+        # Dézipper
+        s, x, y, z = zip(*data_sorted)
+        
+        plt.plot(s, x, label="mse modèle")
+        plt.plot(s, y, label="mse patient")
+        plt.plot(s, z, label="mse docteur")
+
+        # Ajouter des labels et un titre
+        plt.xlabel("sparcité")
+        plt.ylabel("MSE")
+        plt.legend()
+        plt.title("Différents MSE")
+        plt.grid(True)
+
+        # Afficher le graphique
+        plt.show()
 
 
 
